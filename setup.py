def630028463c41105dd0d5aa4f42677700d7b99
Bryan Chow <bryan@fullfactor.com>
2014-08-12 16:13:36 -0700

    Bump version number to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
